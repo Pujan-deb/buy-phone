45530a60b90a1abf43ab9801038cdbdad2c3ab3b
pujan <deb1234.gmail.com>
2023-09-14 14:01:49 +0600

    Buy phone

---
+++ main.js
@@ -0,0 +1,65 @@
+function updatenumberOfcover(isTrue) {
+    let covernum = document.getElementById('cover');
+    let covernumstring = covernum.value;
+    let prevcasenumber = parseInt(covernumstring);
+    let coverprice = document.getElementById('coverprice');
+    let prevprice = coverprice.innerText;
+    let price = parseInt(prevprice);
+    if (isTrue == true) {
+        let newprice = price + 59;
+        coverprice.innerText = newprice;
+        let newcasenumber = prevcasenumber + 1;
+        covernum.value = newcasenumber;
+        return newprice;
+    } else if (isTrue == false) {
+        let newprice = price - 59;
+        coverprice.innerText = newprice;
+        let newcasenumber = prevcasenumber - 1;
+        covernum.value = newcasenumber;
+        return newprice;
+    }
+}
+function updateCoverprice(money) {
+    let phoneprice = document.getElementById('Phoneprice');
+    let phoneprevprice = parseInt(phoneprice.innerText);
+    let subtotal = document.getElementById('subtotal');
+
+    let Newsubtotal = phoneprevprice + money;
+
+    let tax = document.getElementById('tax');
+    let taxamount = Newsubtotal * 0.1;
+    let finaltax = Math.round(taxamount);
+    tax.innerText = finaltax;
+    let finalSubtotal = Newsubtotal + finaltax;
+    subtotal.innerText = Newsubtotal;
+    let total = document.getElementById('total');
+    total.innerText = finalSubtotal;
+}
+document.getElementById('increaseCovernum').addEventListener('click', () => {
+    let price = updatenumberOfcover(true);
+    updateCoverprice(price);
+})
+
+document.getElementById('decreaseCovernum').addEventListener('click', () => {
+    let price = updatenumberOfcover(false);
+    updateCoverprice(price);
+});
+
+document.getElementById('cancel').addEventListener('click', () => {
+    let covernum = document.getElementById('cover');
+    covernum.value = 0;
+    let coverprice = document.getElementById('coverprice');
+    coverprice.innerText = 0;
+    let phoneprice = document.getElementById('Phoneprice').innerText;
+    //tax
+    let tax = document.getElementById('tax');
+    let taxamount = parseInt(phoneprice) * 0.1;
+    let finaltax = Math.round(taxamount);
+    tax.innerText = finaltax;
+    let finalamount = parseInt(phoneprice) + taxamount;
+    let subtotal = document.getElementById('subtotal');
+    subtotal.innerText = phoneprice;
+    let total = document.getElementById('total');
+    total.innerText = finalamount;
+
+})
---
+++ phone.js
@@ -0,0 +1,62 @@
+function updatenumberOfphone(isTrue) {
+    let covernum = document.getElementById('phone');
+    let covernumstring = covernum.value;
+    let prevcasenumber = parseInt(covernumstring);
+    let coverprice = document.getElementById('Phoneprice');
+    let prevprice = coverprice.innerText;
+    let price = parseInt(prevprice);
+    if (isTrue == true) {
+        let newprice = price + 1219;
+        coverprice.innerText = newprice;
+        let newcasenumber = prevcasenumber + 1;
+        covernum.value = newcasenumber;
+        return newprice;
+    } else if (isTrue == false) {
+        let newprice = price - 1219;
+        coverprice.innerText = newprice;
+        let newcasenumber = prevcasenumber - 1;
+        covernum.value = newcasenumber;
+        return newprice;
+    }
+}
+function updatePhoneprice(money) {
+    let phoneprice = document.getElementById('coverprice');
+    let phoneprevprice = parseInt(phoneprice.innerText);
+    let subtotal = document.getElementById('subtotal');
+    let Newsubtotal = phoneprevprice + money;
+    let tax = document.getElementById('tax');
+    let taxamount = Newsubtotal * 0.1;
+    let finaltax = Math.round(taxamount);
+    tax.innerText = finaltax;
+    let finalSubtotal = Newsubtotal + finaltax;
+    subtotal.innerText = Newsubtotal;
+    let total = document.getElementById('total');
+    total.innerText = finalSubtotal;
+}
+document.getElementById('increasephone').addEventListener('click', () => {
+    let money = updatenumberOfphone(true);
+    updatePhoneprice(money);
+})
+document.getElementById('decreasephone').addEventListener('click', () => {
+    let money = updatenumberOfphone(false);
+    updatePhoneprice(money);
+})
+document.getElementById('remove').addEventListener('click', () => {
+    let numberofphone = document.getElementById('phone');
+    numberofphone.value = 0;
+    let priceofphone = document.getElementById('Phoneprice');
+    priceofphone.innerHTML = 0;
+    let coverprice = document.getElementById('coverprice').innerText;
+    //tax
+    let tax = document.getElementById('tax');
+    let taxamount = parseInt(coverprice) * 0.1;
+    let finaltax = Math.round(taxamount);
+    tax.innerText = finaltax;
+    let finalamount = parseInt(coverprice) + taxamount;
+    let subtotal = document.getElementById('subtotal');
+    subtotal.innerText = finalamount;
+    let total = document.getElementById('total');
+    total.innerText = finalamount;
+
+
+})
